aa6cf606b7f1fd56131c9011a573f69d137faf1e
Kosiner0 <beniburdet17@gmail.com>
2024-04-08 13:55:46 +0200

    clases de jugador y equipo2

--- EjerciciosSinensia/src/ejercicio01/Equipo.java
+++ EjerciciosSinensia/src/ejercicio01/Equipo.java
@@ -1,5 +1,48 @@
 package ejercicio01;
 
+import java.util.List;
+
 public class Equipo {
+	
+	protected List<Jugador> jugadores;
+	protected String nombre;
+	protected int puntos;
+	
+	public Equipo(List<Jugador> jugadores, String nombre, int puntos) {
+		super();
+		this.jugadores = jugadores;
+		this.nombre = nombre;
+		this.puntos = puntos;
+	}
+
+	public List<Jugador> getJugadores() {
+		return jugadores;
+	}
+
+	public void setJugadores(List<Jugador> jugadores) {
+		this.jugadores = jugadores;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getPuntos() {
+		return puntos;
+	}
+
+	public void setPuntos(int puntos) {
+		this.puntos = puntos;
+	}
+
+	@Override
+	public String toString() {
+		return "Equipo [jugadores=" + jugadores + ", nombre=" + nombre + ", puntos=" + puntos + "]";
+	}
+	
 
 }
--- EjerciciosSinensia/src/ejercicio01/Jugador.java
+++ EjerciciosSinensia/src/ejercicio01/Jugador.java
@@ -1,5 +1,57 @@
 package ejercicio01;
 
 public class Jugador {
+	
+	protected String nombre;
+	protected int edad;
+	protected String posicion;
+	protected Equipo equipo;
+	
+	public Jugador(String nombre, int edad, String posicion, Equipo equipo) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.posicion = posicion;
+		this.equipo = equipo;
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(String posicion) {
+		this.posicion = posicion;
+	}
+
+	public Equipo getEquipo() {
+		return equipo;
+	}
+
+	public void setEquipo(Equipo equipo) {
+		this.equipo = equipo;
+	}
+
+	@Override
+	public String toString() {
+		return "Jugador [nombre=" + nombre + ", edad=" + edad + ", posicion=" + posicion + ", equipo=" + equipo + "]";
+	}
+	
+	
+	
 }
