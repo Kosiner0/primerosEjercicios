f513a249d2b2e45e409f61b8d136a4eac6904abe
Kosiner0 <beniburdet17@gmail.com>
2024-04-12 11:47:03 +0200

    mejorados ejercicios 7 y 8

--- EjerciciosSinensia/src/ejercicio07/Orquesta.java
+++ EjerciciosSinensia/src/ejercicio07/Orquesta.java
@@ -1,20 +1,40 @@
 package ejercicio07;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Orquesta {
 
 	public static void main(String[] args) {
 		
-		Flauta flauta = new Flauta("Flauton", "Alargada", "Turbo2000");
-		Guitarra guitarra = new Guitarra("Guitarra", "Española", 6);
-		GuitarraElectrica electrica = new GuitarraElectrica("Guitarra", "Electrica", 4, 1200);
-		Tambor tambor = new Tambor("Tambor", "Percusion", 50);
+		//Lista de instrumentos
+		List<Instrumento> instrumentos = new ArrayList<>();
 		
-		flauta.tocar();flauta.afinar();System.out.println();
-		guitarra.tocar();guitarra.afinar();System.out.println();
-		electrica.tocar();electrica.afinar();System.out.println();
-		tambor.aporrear();
-		System.out.println("\n"+ flauta.toString() + "\n" + guitarra.toString() + "\n" + electrica.toString() + "\n" + tambor.toString());
+		//Creando los instrumentos
+		Instrumento flauta = new Flauta("Flauton", "Alargada", "Turbo2000");
+		Instrumento guitarra = new Guitarra("Guitarra", "Española", 6);
+		Instrumento electrica = new GuitarraElectrica("Guitarra", "Electrica", 4, 1200);
+		Instrumento tambor = new Tambor("Tambor", "Percusion", 50);
+		
+		//Añadiendo los instrumentos a la lista
+		instrumentos.add(flauta);
+		instrumentos.add(guitarra);
+		instrumentos.add(electrica);
+		instrumentos.add(tambor);
+		
+		//Iterando sobre la lista para sacar los instrumentos 
+		for(Instrumento instrumento : instrumentos) {
+			if(instrumento instanceof Tambor) {
+				((Tambor) instrumento).aporrear();
+				System.out.println(instrumento.toString() + "\n");
 
+			}else {
+				instrumento.tocar();
+				instrumento.afinar();
+				System.out.println(instrumento.toString() + "\n");
+			}
+		}
+		
 	}
 
 }
