89fc22915e35ff80fd22dc949dc2740460def9c9
Kosiner0 <beniburdet17@gmail.com>
2024-04-16 13:30:22 +0200

    ejercicio 11 hecho

--- EjerciciosSinensia/src/ejercicio11/Electrodomestico.java
+++ EjerciciosSinensia/src/ejercicio11/Electrodomestico.java
@@ -2,32 +2,136 @@ package ejercicio11;
 
 public class Electrodomestico {
 
+	//Atributos por defecto
+	private static final String[] COLORES_VALIDOS = {"blanco", "rojo", "negro", "gris", "azul"};
+	private static final char[] LETRAS_VALIDAS = {'A', 'B', 'C', 'D', 'E', 'F'};
+	private static final String COLOR_BASE = "blanco";
+	private static final double PRECIO_POR_DEFECTO = 100;
+	private static final double PESO_BASE = 5;
+	private static final char CONSUMO_ENERGETICO_BASE = 'F';
+	
+	//Variables de instancia
 	protected double precioBase;
 	protected String color;
 	protected char consumoEnergetico;
 	protected double peso;
 	
-	//Atributos por defecto
-	private static final String[] COLORES_VALIDOS = {"blanco", "rojo", "negro", "gris", "azul"};
-	private static final String COLOR_BASE = "blanco";
-	private static final double PRECIO_BASE = 100;
-	private static final double PESO_BASE = 5;
-	private static final char CONSUMO_ENERGETICO_BASE = 'F';
-	
 	
 	public Electrodomestico() {
-		this.precioBase = PRECIO_BASE;
+		this.precioBase = PRECIO_POR_DEFECTO;
 		this.color = COLOR_BASE;
 		this.consumoEnergetico = CONSUMO_ENERGETICO_BASE;
 		this.peso = PESO_BASE;
 	}
 	
-	public Electrodomestico(double precio, double peso) {
-		this.precioBase = precio;
+	public Electrodomestico(double precioBase, double peso) {
+		this.precioBase = precioBase;
 		this.color = COLOR_BASE;
 		this.consumoEnergetico = CONSUMO_ENERGETICO_BASE;
 		this.peso = peso;
 	}
 	
+	public Electrodomestico(double precioBase, String color, char consumoEnergetico, double peso) {
+		this.precioBase = precioBase;
+		this.color = comprobarColor(color);
+		this.consumoEnergetico = comprobarConsumoEnergetico(consumoEnergetico);
+		this.peso = peso;
+	}
+
+	public double getPrecioBase() {
+		return precioBase;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public char getConsumoEnergetico() {
+		return consumoEnergetico;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	
+	public char comprobarConsumoEnergetico(char letra) {
+		letra = Character.toUpperCase(letra);
+		
+		for(char c : LETRAS_VALIDAS) {
+			if(c == letra) {
+				return letra;
+			}
+		}
+		return CONSUMO_ENERGETICO_BASE;
+	}
+	
+	public String comprobarColor(String color) {
+		
+		for(String c : COLORES_VALIDOS) {
+			if(c.equalsIgnoreCase(color)) {
+				return color;
+			}
+		}
+		return COLOR_BASE;
+	}
+	
+	public double precioFinal() {
+		
+		switch(this.consumoEnergetico) {
+			case 'A':
+				this.precioBase += 100 + precioPorTamanio();
+				break;
+				
+			case 'B':
+				this.precioBase += 80 + precioPorTamanio();
+				break;
+				
+			case 'C':
+				this.precioBase += 60 + precioPorTamanio();
+				break;
+				
+			case 'D':
+				this.precioBase += 50 + precioPorTamanio();
+				break;
+				
+			case 'E':
+				this.precioBase += 30 + precioPorTamanio();
+				break;
+				
+			case 'F':
+				this.precioBase += 10 + precioPorTamanio();
+				break;
+				
+			default:
+				break;
+			
+		}
+		return this.precioBase;
+	}
+	
+	public double precioPorTamanio() {
+		if(this.peso >= 0 && this.peso <= 19) {
+			return (this.precioBase += 10);
+			
+		}else if(this.peso >= 20 && this.peso <= 49) {
+			return (this.precioBase += 50);
+			
+		}else if(this.peso >= 50 && this.peso <= 79) {
+			return (this.precioBase += 80);
+			
+		}else {
+			return (this.precioBase += 100);
+		}
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Electrodomestico [precioBase=" + precioBase + ", color=" + color + ", consumoEnergetico="
+				+ consumoEnergetico + ", peso=" + peso + "]";
+	}
+	
 	
 }
+
---
+++ EjerciciosSinensia/src/ejercicio11/Television.java
@@ -0,0 +1,64 @@
+package ejercicio11;
+
+public class Television extends Electrodomestico{
+
+	private final static double RESOLUCION_BASE = 20;
+	private final static boolean SINTONIZADORtdt_BASE = false;
+	
+	private double resolucion;
+	private boolean sintonizadorTDT;
+	
+	
+	public Television() {
+		super();
+		// TODO Auto-generated constructor stub
+		this.resolucion = RESOLUCION_BASE;
+		this.sintonizadorTDT = SINTONIZADORtdt_BASE;
+	}
+	
+	public Television(double precioBase, double peso) {
+		super(precioBase, peso);
+		// TODO Auto-generated constructor stub
+		this.resolucion = RESOLUCION_BASE;
+		this.sintonizadorTDT = SINTONIZADORtdt_BASE;
+	}
+	
+	public Television(double precioBase, String color, char consumoEnergetico, double peso, double resolucion, boolean sintonizadorTDT) {
+		super(precioBase, color, consumoEnergetico, peso);
+		// TODO Auto-generated constructor stub
+		this.resolucion = resolucion;
+		this.sintonizadorTDT = sintonizadorTDT;
+	}
+
+	public double getResolucion() {
+		return resolucion;
+	}
+
+	public boolean isSintonizadorTDT() {
+		return sintonizadorTDT;
+	}
+
+	@Override
+	public double precioFinal() {
+		// TODO Auto-generated method stub
+		super.precioFinal();
+		
+		if(this.resolucion > 40) {
+			precioBase *= 1.3;
+		}
+		
+		if(this.sintonizadorTDT) {
+			precioBase += 50;
+		}
+		return precioBase;
+	}
+
+	@Override
+	public String toString() {
+		return "Television [resolucion=" + resolucion + ", sintonizadorTDT=" + sintonizadorTDT + ", precioBase="
+				+ precioBase + ", color=" + color + ", consumoEnergetico=" + consumoEnergetico + ", peso=" + peso + "]";
+	}
+	
+	
+}
+
