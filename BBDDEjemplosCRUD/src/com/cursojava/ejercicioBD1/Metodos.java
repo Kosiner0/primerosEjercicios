064e3de967238e5d2784fc6e306f2043842ff260
Kosiner0 <beniburdet17@gmail.com>
2024-04-25 12:07:53 +0200

    hecho ejrcicio 1 de bases de datos

--- BBDDEjemplosCRUD/src/com/cursojava/ejercicioBD1/Metodos.java
+++ BBDDEjemplosCRUD/src/com/cursojava/ejercicioBD1/Metodos.java
@@ -1,5 +1,152 @@
 package com.cursojava.ejercicioBD1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Metodos {
 
+	private static String url = "jdbc:mysql://localhost:3306/ej_bd";
+	private static String user = "root";
+	private static String password = "root";
+	
+	
+	public static boolean buscarCodigo(int codigo) {
+		String query = "select * from empleados";
+		
+		try (Connection conexion = DriverManager.getConnection(url, user, password);
+				Statement sentencia = conexion.createStatement();
+				ResultSet rs = sentencia.executeQuery(query);
+			){
+			
+			while(rs.next()) {
+				if(rs.getInt("idempleado") == codigo) {
+					return true;
+				}
+				
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public static boolean insertarEmpleado(int codigo, String nombre, String apellido, String fechaNacimiento, double salario) {
+		boolean insertado = false;
+		String query = "insert into empleados values ("+ codigo +", '"+ nombre +"',"
+				+ " '"+ apellido +"', '"+ fechaNacimiento +"', "+ salario +")";
+		
+		try (Connection conexion = DriverManager.getConnection(url, user, password);
+				Statement sentencia = conexion.createStatement();
+				){
+			
+			sentencia.executeUpdate(query);
+			insertado = true;
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return insertado;
+	}
+	
+	public static boolean actualizarNombreUsuario(int codigo, String nombre) {
+		boolean actualizado = false;
+		String query = "update empleados set nombre = '"+ nombre +"' WHERE idempleado = "+ codigo +"";
+		
+		try (Connection conexion = DriverManager.getConnection(url, user, password);
+				Statement sentencia = conexion.createStatement();
+			){
+			sentencia.execute(query);
+			actualizado = true;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return actualizado;
+	}
+	
+	public static void mostrarDatos() {
+		String query = "select * from empleados";
+		
+		try (Connection conexion = DriverManager.getConnection(url, user, password);
+				Statement sentencia = conexion.createStatement();
+				ResultSet rs = sentencia.executeQuery(query);
+			){
+			
+			while(rs.next()) {
+				System.out.print("Id empleado=" + rs.getInt("idempleado"));
+				System.out.print(", ");
+				System.out.print("nombre=" + rs.getString("nombre"));
+				System.out.print(", ");
+				System.out.print("apellido=" + rs.getString("apellido"));
+				System.out.print(", ");
+				System.out.print("fecha nacimiento=" + rs.getString("fecha_nacimiento"));
+				System.out.print(", ");
+				System.out.print("salario=" + rs.getDouble("salario"));
+				System.out.println();
+				
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void mostrarDatosPorSalario() {
+		String query = "select * from empleados WHERE salario > 1800";
+		
+		try (Connection conexion = DriverManager.getConnection(url, user, password);
+				Statement sentencia = conexion.createStatement();
+				ResultSet rs = sentencia.executeQuery(query);
+			){
+			
+			while(rs.next()) {
+				System.out.print("Id empleado=" + rs.getInt("idempleado"));
+				System.out.print(", ");
+				System.out.print("nombre=" + rs.getString("nombre"));
+				System.out.print(", ");
+				System.out.print("apellido=" + rs.getString("apellido"));
+				System.out.print(", ");
+				System.out.print("fecha nacimiento=" + rs.getString("fecha_nacimiento"));
+				System.out.print(", ");
+				System.out.print("salario=" + rs.getDouble("salario"));
+				System.out.println();
+				
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static boolean actualizarUsuario(int codigo, String nombre, String apellido, String fechaNacimiento, double salario) {
+		boolean actualizado = false;
+		String query = "update empleados set nombre = ?, apellido = ?, fecha_nacimiento = ?, salario = ? WHERE idempleado = ? ";
+		
+		try (Connection conexion = DriverManager.getConnection(url, user, password);
+				PreparedStatement sentencia = conexion.prepareStatement(query);
+			){
+			
+			sentencia.setString(1, nombre);
+			sentencia.setString(2, apellido);
+			sentencia.setString(3, fechaNacimiento);
+			sentencia.setDouble(4, salario);
+			sentencia.setInt(5, codigo);
+			
+			if(sentencia.executeUpdate() > 0) {
+				actualizado = true;
+			}
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return actualizado;
+	}
+	
 }
--- BBDDEjemplosCRUD/src/com/cursojava/ejercicioBD1/Principal.java
+++ BBDDEjemplosCRUD/src/com/cursojava/ejercicioBD1/Principal.java
@@ -1,10 +1,102 @@
 package com.cursojava.ejercicioBD1;
 
+import entrada.Teclado;
+
 public class Principal {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static void menu() {
+		System.out.println("\n0-Salir del programa.");
+		System.out.println("1-Insertar usuario.");
+		System.out.println("2-Actualizar nombre de usuario.");
+		System.out.println("3-Mostrar datos de usuario.");
+		System.out.println("4-Mostrar usuarios con salario > 1800.");
+		System.out.println("5-Actualizar datos de un usuario.");
 	}
 
+	public static void main(String[] args) {
+		int opcion;
+		
+		do {
+			
+			menu();
+			opcion = Teclado.leerEntero("\n--Introduce la opcion: ");
+			
+			switch(opcion) {
+					
+				case 0:
+					break;
+				
+				case 1:
+					int codigo = Teclado.leerEntero("Introduce el código del empleado: ");
+					if(Metodos.buscarCodigo(codigo)) {
+						System.out.println("Ya existe un empleado con ese código.");
+						
+					}else {
+						String nombre = Teclado.leerCadena("Introduce el nombre del empleado: ");
+						String apellido = Teclado.leerCadena("Introduce el apellido del empleado: ");
+						String fechaNacimiento = Teclado.leerCadena("Introduce la fecha de nacimiento del empleado: ");
+						double salario = Teclado.leerReal("Introduce el salario del empleado: ");
+						
+						boolean insertado = Metodos.insertarEmpleado(codigo, nombre, apellido, fechaNacimiento, salario);
+						if(insertado) {
+							System.out.println("Se ha insertado un empleado en la base de datos.");
+						}
+						
+					}
+					break;
+					
+				case 2:
+					codigo = Teclado.leerEntero("Introduce el código del empleado: ");
+					
+					if(Metodos.buscarCodigo(codigo)) {
+						String nombre = Teclado.leerCadena("Introduce el nombre actualizado del empleado: ");
+						
+						boolean actualizado = Metodos.actualizarNombreUsuario(codigo, nombre);
+						if(actualizado) {
+							System.out.println("Se ha actualizado el usuario.");
+						}
+						
+					}else {
+						System.out.println("No existe un empleado con ese código.");
+					}
+					break;
+					
+				case 3:
+					Metodos.mostrarDatos();
+					
+					break;
+					
+				case 4:
+					Metodos.mostrarDatosPorSalario();
+					
+					break;
+					
+				case 5:
+					codigo = Teclado.leerEntero("Introduce el código del empleado: ");
+					
+					if(Metodos.buscarCodigo(codigo)) {
+						String nombre = Teclado.leerCadena("Introduce el nombre actualizado del empleado: ");
+						String apellido = Teclado.leerCadena("Introduce el apellido actualizado del empleado: ");
+						String fechaNacimiento = Teclado.leerCadena("Introduce la fecha de nacimiento actualizada del empleado: ");
+						double salario = Teclado.leerReal("Introduce el salario actualizado del empleado: ");
+						
+						boolean actualizado = Metodos.actualizarUsuario(codigo, nombre, apellido, fechaNacimiento, salario);
+						if(actualizado) {
+							System.out.println("Se ha actualizado el usuario.");
+						}
+						
+					}else {
+						System.out.println("No existe un empleado con ese código.");
+					}
+					break;
+					
+				default:
+					System.out.println("La opción debe estar comprendida entre 0 y 5.");
+					break;
+			}
+
+		}while(opcion != 0);
+		System.out.println("Fin de programa.");
+			
+	}
 }
